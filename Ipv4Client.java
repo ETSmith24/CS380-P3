a1ff993040dc7f92aa9232f18fc6ec6df4ee9067
Ethan <etsmith@cpp.edu>
2017-10-26 20:43:17 -0700

    fixed output and some code formating

--- Ipv4Client.java
+++ Ipv4Client.java
@@ -1,4 +1,4 @@
-x// ============================================================================
+// ============================================================================
 // file: Ipv4Client.java
 // ============================================================================
 // Programmers: David Shin & Ethan Smith
@@ -15,36 +15,89 @@ x// ============================================================================
 import java.io.IOException;
 import java.net.Socket;
 import java.lang.Math;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+
 public class Ipv4Client
 {
     public static void main(String[] args) {
-	//public static int LEN = 2;
 	Socket socket;
-	byte[] version;
-	byte[] HLen;
-	byte[] length;
 	byte[] data; 
 	    
-
-	
 	try{
 	    socket = new Socket("18.221.102.182", 38003);
 	    System.out.println("Connected to server");
-
-
+	    
+	    //setting up input stream
+	    InputStream is = socket.getInputStream();
+	    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+	    BufferedReader br = new BufferedReader(isr);
+	    
+	    //setting up output stream
+	    OutputStream os = socket.getOutputStream();
+	    PrintStream ps = new PrintStream(os);
+	    
 	    for(int i = 1; i <= 12; i++)
 		{
 		    //creates a new byte array for data of length 2,4,8,16,32,64,128,256,512,1024,2048
-		    data = new byte[(int)Math.pow(2.0, i)];
+		    data = new byte[(int)Math.pow(2.0, i) + 20];
+		    //FrameFill returns a byte array in IPv4 format
+		    data = FrameFill(data);
+		    ps.write(data);
 		    System.out.println("data length :" + data.length);
+		    System.out.println(br.readLine() + '\n');
 		}
 
-	}
-	catch(IOException e)
-	    {
+	} catch(IOException e) {
 		e.printStackTrace();
-	    }  
+	}  
+	}//end main
+
+    public static byte[] FrameFill(byte[] data) {
+	short length = (short) data.length;
+	data[0] = 0x45; //version 4, header length 5
+	data[1] = 0x0;//type of service
+	data[2] = (byte) ((length >> 8) & 0xFF);//Header + Data(0)?
+	data[3] = (byte) (length & 0xFF); //20+2*
+
+
+	data[4] = 0x0; //identifier
+
+	data[5] = 0x0;
+	data[6] = (0x1 << 6); //flag : fragment
+
+	data[7] = 0x0; //offset
+	data[8] = 0x32; // 50 TTL
+	data[9] = 0x6; // TCP = 6
+	data[10] = 0x0; //insert verified Checksum
+	data[11] = 0x0;
+
+	data[12] = (byte) 72; //random source address
+	data[13] = (byte) 182;
+	data[14] = (byte) 13;
+	data[15] = (byte) 171;
+
+	data[16] = (byte) 18;//destination inet address: 18.221.102.182
+	data[17] = (byte) 221;
+	data[18] = (byte) 102;
+	data[19] = (byte) 182;
+
+	//insert verified checksum
+	short CheckedSum = checksum(data);
+	data[10] = (byte) ((CheckedSum >> 8) & 0xFF);
+	data[11] = (byte) (CheckedSum & 0xFF);
+
+
+	// Data section is 0
+	for (int i = 20; i < length; ++i)
+	    data[i] = 0x0;
+	return data;
     }
+    
+   
 	
 	public static short checksum(byte[] b) {
 		int length = b.length;
